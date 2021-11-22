26459d54f199d5c55f5ac3a55201496c3039a299
uplus-dvc <shqlsths12@naver.com>
2021-11-22 05:44:26 +0000

    mlops

--- week_7_ecr/app.py
+++ week_7_ecr/app.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from inference_onnx import ColaONNXPredictor
-app = FastAPI(title="MLOps Basics App")
+app = FastAPI(title="uplus MLOps Basics App")
 
 predictor = ColaONNXPredictor("./models/model.onnx")
 
 @app.get("/")
 async def home_page():
-    return "<h2>Sample prediction API</h2>"
+    return "<h2>uplus prediction API</h2>"
 
 
 @app.get("/predict")
